44138bf878829d48da591695c587458b24ec4286
LeoGD1 <bridonegd@outlook.com>
2025-08-03 21:01:17 -0600

    primer despliegue de cancionero

---
+++ src/data/data.js
@@ -0,0 +1,103 @@
+const datos = [
+  {
+    fondo: './media/fondos/1.jpg',
+    cd: './media/discos/d1.png',
+    titulo: 'Muchachos',
+    autor: 'La banda del rojo',
+    enlace: '/muchachos',
+    ilustrador: "Rene Sarek",
+    instagram: 'https://www.instagram.com/rene_sarek/'
+  },
+  {
+    fondo: './media/fondos/6.jpg',
+    cd: './media/discos/d6.png',
+    titulo: 'Matador',
+    autor: 'La banda del rojo',
+    enlace: '/matador',
+    ilustrador: "Rene Sarek",
+    instagram: 'https://www.instagram.com/rene_sarek/'
+  },
+  {
+    fondo: './media/fondos/2.jpg',
+    cd: './media/discos/d2.png',
+    titulo: 'Yo te pido un favor',
+    autor: 'La banda del rojo',
+    enlace: '/yo-te-pido-un-favor',
+    ilustrador: "Rene Sarek",
+    instagram: 'https://www.instagram.com/rene_sarek/'
+  },
+  {
+    fondo: './media/fondos/3.jpg',
+    cd: './media/discos/d3.png',
+    titulo: 'Se viene la banda',
+    autor: 'La banda del rojo',
+    enlace: '/se-viene-la-banda',
+    ilustrador: "Rene Sarek",
+    instagram: 'https://www.instagram.com/rene_sarek/'
+  },
+  {
+    fondo: './media/fondos/4.jpg',
+    cd: './media/discos/d4.png',
+    titulo: 'Cumbia de los trapos',
+    autor: 'Yerba brava',
+    enlace: '/cumbia-de-los-trapos',
+    ilustrador: "Rene Sarek",
+    instagram: 'https://www.instagram.com/rene_sarek/'
+  },
+  {
+    fondo: './media/fondos/5.jpg',
+    cd: './media/discos/d5.png',
+    titulo: 'Asi se dio',
+    autor: 'La banda del rojo',
+    enlace: '/asi-se-dio',
+    ilustrador: "Rene Sarek",
+    instagram: 'https://www.instagram.com/rene_sarek/'
+  },
+  {
+    fondo: './media/fondos/7.jpg',
+    cd: './media/discos/d7.png',
+    titulo: 'Con el corazon',
+    autor: 'La banda del rojo',
+    enlace: '/con-el-corazon',
+    ilustrador: "Rene Sarek",
+    instagram: 'https://www.instagram.com/rene_sarek/'
+  },
+  {
+    fondo: './media/fondos/8.jpg',
+    cd: './media/discos/d8.png',
+    titulo: 'Te venimos a ver',
+    autor: 'La banda del rojo',
+    enlace: '/te-venimos-a-ver',
+    ilustrador: "Rene Sarek",
+    instagram: 'https://www.instagram.com/rene_sarek/'
+  },
+  {
+    fondo: './media/fondos/9.jpg',
+    cd: './media/discos/d9.png',
+    titulo: 'Solo vivo para ti',
+    autor: 'La banda del rojo',
+    enlace: '/solo-vivo-para-ti',
+    ilustrador: "Rene Sarek",
+    instagram: 'https://www.instagram.com/rene_sarek/'
+  },
+  {
+    fondo: './media/fondos/10.jpg',
+    cd: './media/discos/d10.png',
+    titulo: 'Yo soy asi',
+    autor: 'La banda del rojo',
+    enlace: '/yo-soy-asi',
+    ilustrador: "Rene Sarek",
+    instagram: 'https://www.instagram.com/rene_sarek/'
+  },
+  {
+    fondo: './media/fondos/11.jpg',
+    cd: './media/discos/d11.png',
+    titulo: 'Te sigo a todas partes',
+    autor: 'La banda del rojo',
+    enlace: '/te-sigo-a-todas-partes',
+    ilustrador: "Rene Sarek",
+    instagram: 'https://www.instagram.com/rene_sarek/'
+  },
+];
+
+export default datos;
